0e231a0c60c0aa9035f3e17cf8bb19dc0362fd5c
Saurabh Yadav <74822612+HakashiKatake@users.noreply.github.com>
2025-02-05 20:28:56 +0530

    db connection done using neon

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import morgan from "morgan";
 import dotenv from 'dotenv';
 import cors from 'cors';
 import productRoutes from './routes/productRoutes.js';
-
+import { sql } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,7 +18,29 @@ app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port '+ PORT);
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price NUMERIC(10, 2) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+        console.log("Database is connected");
+    } catch (error) {
+        console.log("Error initDB", error);
+    }
+}
+
+initDB().then(() => {
+    app.listen(3000, () => {
+        console.log('Server is running on port '+ PORT);
     });
+});
+
+
+
 
